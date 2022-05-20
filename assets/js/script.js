f1af76dca28eaa01eca6e60c7610c5c8e024207e
Gabriel <eualvesgabriel@gmail.com>
2022-05-19 21:17:24 -0300

    Atualizando

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,30 @@
-var currentNumberWrapper = document.getElementById('currentNumber'); //armazenando aquele valor
-var currentNumber = 0;
-function increment() {
-    currentNumber = currentNumber + 1;
-    currentNumberWrapper.innerHTML = currentNumber; // precisei colocar o inner.html, para alterar dentro do HTML, se não, não teriamos como visualizar na index
-}
+let currentNumberWrapper = document.getElementById("currentNumber")
+const increment =  document.getElementById("increment")
+const decrement =  document.getElementById("decrement")
+let count = 0;
 
-function decrement() {
-    currentNumber = currentNumber - 1;
-    currentNumberWrapper.innerHTML = currentNumber;
+increment.addEventListener("click",
+    function increment() {
+        count++;
+        if(count <=10){
+            currentNumberWrapper.innerHTML = count;
+        }
 
-}
+        if (count >= 0){
+            currentNumberWrapper.classList.remove("negative")
+        }
+    }
+)
+
+decrement.addEventListener("click",
+    function decrement() {
+        count--;
+        if(count >= 10){
+            currentNumberWrapper.innerHTML = count;
+        }
+
+        if(count < 0){
+            currentNumberWrapper.classList.add("negative")
+        }
+    }
+)    
